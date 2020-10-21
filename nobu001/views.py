701ba88be14bda0cc271905bf6c9be674b78e968
Katuhito Hara <katuhitohara@gmail.com>
2020-10-21 11:34:22 +0900

    index url static css

--- nobu001/views.py
+++ nobu001/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request, nickname, age):
-    result = 'your account: ' + nickname + '"(' + str(age) + ').'
+def index(request):
+    params = {
+        'title':'Hello/Index',
+        'msg': 'これは、サンプルページ。',
+        'goto':'next',
+    }
+    return render(request, 'nobu001/index.html', params)
+
+def next(request):
+    params = {
+        'title':'Hello/Next',
+        'msg':'これは、もう一つのページです。',
+        'goto':'index',
+    }
+    return render(request, 'nobu001/index.html', params)
     
-    return HttpResponse(result)
+
 
 
 
