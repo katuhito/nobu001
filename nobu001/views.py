b97b704acfa6e2444c742ba211d0e167e4c5a3cd
Katuhito Hara <katuhitohara@gmail.com>
2020-11-04 14:17:34 +0900

    CRUD

--- nobu001/views.py
+++ nobu001/views.py
@@ -1,27 +1,71 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from .models import Friend
-from django.db.models import QuerySet
-
-def __new_str__(self):
-    result = ''
-    for item in self:
-        result += '<tr>'
-        for k in item:
-            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
-        result += '</tr>'
-    return result
-
-QuerySet.__str__ = __new_str__
+# from .forms import HelloForm
+from .forms import FriendForm
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 
 def index(request):
-    data = Friend.objects.all().values('id', 'name', 'age')
+    data = Friend.objects.all()
     params = {
         'title': 'Hello',
         'data': data,
     }
     return render(request, 'nobu001/index.html', params)
+
+# create model
+def create(request):
+    params = {
+        'title': 'Hello',
+        'form': FriendForm()
+    }
+    if (request.method == 'POST'):
+        obj = Friend()
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/nobu001')
+    params = {
+        'title': 'Hello',
+        'form': FriendForm()
+    }
+    return render(request, 'nobu001/create.html', params)
+
+def edit(request, num):
+    obj = Friend.objects.get(id=num)
+    if (request.method == 'POST'):
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/nobu001')
+    params = {
+        'title': 'Hello',
+        'id': num,
+        'form': FriendForm(instance=obj),
+    }
+    return render(request, 'nobu001/edit.html', params)
+
+def delete(request, num):
+    friend = Friend.objects.get(id=num)
+    if (request.method == 'POST'):
+        friend.delete()
+        return redirect(to='/nobu001')
+    params = {
+        'title': 'Hello',
+        'id': num,
+        'obj': friend,
+    }
+    return render(request, 'nobu001/delete.html', params)
+
+class FriendList(ListView):
+    model = Friend
+
+class FriendDetail(DetailView):
+    model = Friend
+
+
+
 
     
     
