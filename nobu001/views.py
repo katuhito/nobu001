8f589c1c6cf416d7a37c170e55ddf0b7affb4e66
Katuhito Hara <katuhitohara@gmail.com>
2020-11-09 16:40:53 +0900

    Record Control SQL Query

--- nobu001/views.py
+++ nobu001/views.py
@@ -7,11 +7,22 @@ from .forms import FriendForm
 from django.views.generic import ListView
 from django.views.generic import DetailView
 from .forms import FindForm
+from django.db.models import Count,Sum,Avg,Min,Max
 
 def index(request):
     data = Friend.objects.all()
+    re1 = Friend.objects.aggregate(Count('age'))
+    re2 = Friend.objects.aggregate(Sum('age'))
+    re3 = Friend.objects.aggregate(Avg('age'))
+    re4 = Friend.objects.aggregate(Min('age'))
+    re5 = Friend.objects.aggregate(Max('age'))
+    msg = 'count:' + str(re1['age__count']) + '<br>Sum:' + str(re2['age__sum']) \
+        + '<br>Average:' + str(re3['age__avg']) + '<br>Min:' + str(re4['age__min']) \
+            + '<br>Max:' + str(re5['age__max'])
+
     params = {
         'title': 'Hello',
+        'message': msg,
         'data': data,
     }
     return render(request, 'nobu001/index.html', params)
@@ -64,12 +75,16 @@ class FriendList(ListView):
 class FriendDetail(DetailView):
     model = Friend
 
+
 def find(request):
     if (request.method == 'POST'):
+        msg = request.POST['find']
         form = FindForm(request.POST)
-        find = request.POST['find']
-        data = Friend.objects.filter(name__icontains=find)
-        msg = 'Result: ' + str(data.count())
+        sql = 'select * from nobu001_friend'
+        if (msg != ''):
+            sql += ' where ' + msg
+        data = Friend.objects.raw(sql)
+        msg = sql
     else:
         msg = 'search words...'
         form = FindForm()
